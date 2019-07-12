25077ead12397424c35c41253dbb808919801373
haniwaman <haniwa008@gmail.com>
2019-07-12 19:37:12 +0900

    ドロワーパーツ追加

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,23 @@ jQuery('.c-file-control input[type="file"]').change(function() {
 	jQuery(".c-file-result").show();
 	jQuery(".c-file-result").text(jQuery(this).val());
 });
+
+// .c-drawer > .e-hamburger
+jQuery(".c-drawer > .e-hamburger, .c-drawer > .e-close").on("click", function() {
+	jQuery(this)
+		.parent(".c-drawer")
+		.toggleClass("is-checked");
+	if (
+		jQuery(this)
+			.parent(".c-drawer")
+			.hasClass("is-checked")
+	) {
+		jQuery(this)
+			.children(".e-text")
+			.text("CLOSE");
+	} else {
+		jQuery(this)
+			.children(".e-text")
+			.text("MENU");
+	}
+});
