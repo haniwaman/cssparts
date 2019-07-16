996827bf190cf4167e533a831e1cd1af3ba64579
haniwaman <haniwa008@gmail.com>
2019-07-16 19:11:50 +0900

    Swiper、hover、パララックス追加

--- src/js/script.js
+++ src/js/script.js
@@ -75,3 +75,52 @@ jQuery('a[href^="#"]').click(function() {
 	);
 	return false;
 });
+
+// スクロールしたら処理
+jQuery(window).on("scroll", function() {
+	if (100 < jQuery(this).scrollTop()) {
+		jQuery(".l-header").addClass("is-scroll");
+	} else {
+		jQuery(".l-header").removeClass("is-scroll");
+	}
+});
+
+// パララックス
+jQuery(window).on("load scroll", function() {
+	var elemAnim = jQuery(".js-anim");
+	var isAnimate = "is-anim";
+
+	elemAnim.each(function() {
+		var winScroll = jQuery(window).scrollTop();
+		var winHeight = jQuery(window).height();
+		var scrollPos = winScroll + winHeight;
+		var buffer = 100; // 表示領域に入ってからクラス付与までの距離
+
+		if (jQuery(this).offset().top < scrollPos - buffer) {
+			jQuery(this).addClass(isAnimate);
+		}
+	});
+});
+
+// Swiper
+var swiper = new Swiper(".swiper-container", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	},
+	loop: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: true
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		type: "bullets",
+		clickable: true
+	},
+	speed: 300,
+	slidesPerView: 1,
+	spaceBetween: 0,
+	initialSlide: 0,
+	effect: "slide" // fade
+});
