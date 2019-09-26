79d90a73f0d95f228ef55007f7fb3eb98b2d55dd
haniwaman <haniwa008@gmail.com>
2019-09-26 21:13:18 +0900

    スクロール判定に.js-mvを追加

--- src/js/script.js
+++ src/js/script.js
@@ -1,103 +1,111 @@
 jQuery(function() {
+	let length = 100;
+	if (jQuery(document).hasClass('js-mv')) {
+		length = jQuery('.js-mv').innerHeight();
+	}
 	// スクロール判定
-	jQuery(window).on("scroll", function() {
-		if (100 < jQuery(this).scrollTop()) {
-			jQuery("body").attr("data-scroll", "true");
+	jQuery(window).on('scroll', function() {
+		let length = 100;
+		if (jQuery('.js-mv').length) {
+			length = jQuery('.js-mv').innerHeight();
+		}
+		if (length < jQuery(this).scrollTop()) {
+			jQuery('body').attr('data-scroll', 'true');
 		} else {
-			jQuery("body").attr("data-scroll", "false");
+			jQuery('body').attr('data-scroll', 'false');
 		}
 	});
 
 	// ローディング判定
-	jQuery("body")
+	jQuery('body')
 		.delay(3000)
 		.queue(function() {
 			jQuery(this)
-				.attr("data-loading", "true")
+				.attr('data-loading', 'true')
 				.dequeue();
 		});
-	jQuery(window).on("load", function() {
-		jQuery("body").attr("data-loading", "true");
+	jQuery(window).on('load', function() {
+		jQuery('body').attr('data-loading', 'true');
 	});
 
 	// .js-file
-	jQuery(".js-file").change(function() {
+	jQuery('.js-file').change(function() {
 		jQuery(this)
-			.siblings(".c-file__result")
+			.siblings('.c-file__result')
 			.show();
 		jQuery(this)
-			.siblings(".c-file__result")
+			.siblings('.c-file__result')
 			.text(jQuery(this).val());
 	});
 
 	// .js-drawer
-	jQuery(".js-drawer").on("click", function(e) {
+	jQuery('.js-drawer').on('click', function(e) {
 		e.preventDefault();
-		let targetClass = jQuery(this).attr("data-target");
-		jQuery("." + targetClass).toggleClass("is-checked");
+		let targetClass = jQuery(this).attr('data-target');
+		jQuery('.' + targetClass).toggleClass('is-checked');
 		return false;
 	});
 
-	jQuery(".js-drawer-close a").on("click", function(e) {
+	jQuery('.js-drawer-close a').on('click', function(e) {
 		e.preventDefault();
 		jQuery(this)
-			.parents(".js-drawer-close")
+			.parents('.js-drawer-close')
 			.parent()
-			.toggleClass("is-checked");
+			.toggleClass('is-checked');
 		return false;
 	});
 
 	// .js-modal
-	jQuery(".js-modal").on("click", function(e) {
+	jQuery('.js-modal').on('click', function(e) {
 		e.preventDefault();
-		let target = jQuery(this).attr("data-target");
-		jQuery("#" + target).toggleClass("is-checked");
+		let target = jQuery(this).attr('data-target');
+		jQuery('#' + target).toggleClass('is-checked');
 		return false;
 	});
 
 	// .js-accordion
-	jQuery(".js-accordion").on("click", function(e) {
+	jQuery('.js-accordion').on('click', function(e) {
 		e.preventDefault();
 		jQuery(this)
-			.siblings(".js-accordion-content")
+			.siblings('.js-accordion-content')
 			.slideToggle(500);
 		jQuery(this)
-			.parent(".c-accordion")
-			.toggleClass("is-checked");
+			.parent('.c-accordion')
+			.toggleClass('is-checked');
 		return false;
 	});
 
 	// .js-tab
-	jQuery(".js-tab").on("click", function(e) {
+	jQuery('.js-tab').on('click', function(e) {
 		e.preventDefault();
 		jQuery(this)
 			.parent()
 			.children()
-			.removeClass("is-checked");
-		jQuery(this).addClass("is-checked");
-		let target = jQuery(this).attr("data-target");
-		jQuery("#" + target)
+			.removeClass('is-checked');
+		jQuery(this).addClass('is-checked');
+		let target = jQuery(this).attr('data-target');
+		jQuery('#' + target)
 			.parent()
 			.children()
-			.removeClass("is-checked");
-		jQuery("#" + target).addClass("is-checked");
+			.removeClass('is-checked');
+		jQuery('#' + target).addClass('is-checked');
 		return false;
 	});
 
 	// スムーススクロール
 	jQuery('a[href^="#"]').click(function() {
-		let header = jQuery(".l-header").height();
+		let header = jQuery('.l-header').height();
 		let speed = 300;
-		let id = jQuery(this).attr("href");
-		let target = jQuery("#" == id ? "html" : id);
+		let id = jQuery(this).attr('href');
+		let target = jQuery('#' == id ? 'html' : id);
 		let position = jQuery(target).offset().top - header;
-		if ("fixed" !== jQuery(".l-header").css("position")) {
+		if ('fixed' !== jQuery('.l-header').css('position')) {
 			position = jQuery(target).offset().top;
 		}
 		if (0 > position) {
 			position = 0;
 		}
-		jQuery("html, body").animate(
+		jQuery('html, body').animate(
 			{
 				scrollTop: position
 			},
@@ -107,13 +115,13 @@ jQuery(function() {
 	});
 
 	// パララックス
-	const parallaxOptions = { root: null, rootMargin: "0px", threshold: [0.25] };
-	var parallaxItems = [].slice.call(document.querySelectorAll(".js-anim"));
+	const parallaxOptions = { root: null, rootMargin: '0px', threshold: [0.25] };
+	var parallaxItems = [].slice.call(document.querySelectorAll('.js-anim'));
 	let parallaxItemObserver = new IntersectionObserver(function(entries, observer) {
 		entries.forEach(function(entry) {
 			if (entry.isIntersecting) {
 				let parallaxItem = entry.target;
-				parallaxItem.classList.add("is-anim");
+				parallaxItem.classList.add('is-anim');
 				parallaxItemObserver.unobserve(parallaxItem);
 			}
 		});
@@ -125,38 +133,38 @@ jQuery(function() {
 
 	// 電話番号
 	let userAgent = navigator.userAgent;
-	if (userAgent.indexOf("iPhone") < 0 && userAgent.indexOf("Android") < 0) {
+	if (userAgent.indexOf('iPhone') < 0 && userAgent.indexOf('Android') < 0) {
 		jQuery('a[href^="tel:"]')
-			.css("cursor", "default")
-			.on("click", function(e) {
+			.css('cursor', 'default')
+			.on('click', function(e) {
 				e.preventDefault();
 			});
 	}
 
 	// StickyのIE11対応
 	// https://blog.hiroyuki90.com/demo/position-sticky/stickyfill.min.js
-	let sticky = document.querySelectorAll(".l-section--sticky");
+	let sticky = document.querySelectorAll('.l-section--sticky');
 	Stickyfill.add(sticky);
 
 	// LazyLoad
 	// https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/
-	const lazyOptions = { root: null, rootMargin: "0px", threshold: [0] };
-	var lazyItems = [].slice.call(document.querySelectorAll(".js-lazy"));
+	const lazyOptions = { root: null, rootMargin: '0px', threshold: [0] };
+	var lazyItems = [].slice.call(document.querySelectorAll('.js-lazy'));
 	lazyItems.forEach(function(lazyItem) {
-		lazyItem.setAttribute("data-src", lazyItem.src);
-		lazyItem.src = "";
+		lazyItem.setAttribute('data-src', lazyItem.src);
+		lazyItem.src = '';
 	});
 	let lazyItemObserver = new IntersectionObserver(function(entries, observer) {
 		entries.forEach(function(entry) {
 			if (entry.isIntersecting) {
 				let lazyItem = entry.target;
-				if (lazyItem.dataset.hasOwnProperty("src")) {
+				if (lazyItem.dataset.hasOwnProperty('src')) {
 					lazyItem.src = lazyItem.dataset.src;
 				}
-				if (lazyItem.dataset.hasOwnProperty("srcset")) {
+				if (lazyItem.dataset.hasOwnProperty('srcset')) {
 					lazyItem.srcset = lazyItem.dataset.srcset;
 				}
-				lazyItem.classList.remove("js-lazy");
+				lazyItem.classList.remove('js-lazy');
 				lazyItemObserver.unobserve(lazyItem);
 			}
 		});
